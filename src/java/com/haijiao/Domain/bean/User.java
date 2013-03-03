9b8efc659eeb73756af8320317b31e09a0793805
yx <qqfishyx@gmail.com>
2013-03-03 21:06:04 +0800

    solve conflict

--- src/java/com/haijiao/Domain/bean/User.java
+++ src/java/com/haijiao/Domain/bean/User.java
@@ -12,12 +12,8 @@ public class User {
     protected String userId;
     protected String account;
     protected String name;
-<<<<<<< HEAD
     protected String userType;
     protected List<DataFile> files;
-=======
-    protected List<UserFileGroup> fileGroups;
->>>>>>> bookmark complete
     
     public String getUserId() {
         return userId;
@@ -35,64 +31,64 @@ public class User {
         this.account = account;
     } 
     
-    public void addFileGroupGroup(String groupName){
-        UserFileGroup group = new UserFileGroup(groupName);
-        fileGroups.add(group);
-    }
-    
-    public boolean addFile(String groupName, DataFile file){
-        UserFileGroup group = null;
-        for(int i = 0; i < fileGroups.size(); i++){
-            if(fileGroups.get(i).getGroupName().equals(groupName)){
-                group = fileGroups.get(i);
-                break;
-            }
-        }
-        if(group == null){
-            return false;
-        }
-        
-        group.addFile(file);
-        return true;
-    }
-    
-    public void removeFile(String groupName, DataFile file){
-        UserFileGroup group = null;
-        for(int i = 0; i < fileGroups.size(); i++){
-            if(fileGroups.get(i).getGroupName().equals(groupName)){
-                group = fileGroups.get(i);
-                break;
-            }
-        }
-        if(group == null){
-            return;
-        }
-        
-        group.removeFile(file);
-    }
-    
-    public void moveFile(String fromGroup, String toGroup, DataFile file){
-        UserFileGroup from = null;
-        UserFileGroup to = null;
-        for(int i = 0; i < fileGroups.size(); i++){
-            if(fileGroups.get(i).getGroupName().equals(fromGroup)){
-                from = fileGroups.get(i);
-            }
-            if(fileGroups.get(i).getGroupName().equals(toGroup)){
-                to = fileGroups.get(i);
-            }
-        }
-        if(from == null || to == null){
-            return;
-        }
-        
-        from.removeFile(file);
-        to.addFile(file);
-    }
-
-    public List<UserFileGroup> getFileGroups() {
-        return fileGroups;
-    }
+//    public void addFileGroupGroup(String groupName){
+//        UserFileGroup group = new UserFileGroup(groupName);
+//        fileGroups.add(group);
+//    }
+//    
+//    public boolean addFile(String groupName, DataFile file){
+//        UserFileGroup group = null;
+//        for(int i = 0; i < fileGroups.size(); i++){
+//            if(fileGroups.get(i).getGroupName().equals(groupName)){
+//                group = fileGroups.get(i);
+//                break;
+//            }
+//        }
+//        if(group == null){
+//            return false;
+//        }
+//        
+//        group.addFile(file);
+//        return true;
+//    }
+//    
+//    public void removeFile(String groupName, DataFile file){
+//        UserFileGroup group = null;
+//        for(int i = 0; i < fileGroups.size(); i++){
+//            if(fileGroups.get(i).getGroupName().equals(groupName)){
+//                group = fileGroups.get(i);
+//                break;
+//            }
+//        }
+//        if(group == null){
+//            return;
+//        }
+//        
+//        group.removeFile(file);
+//    }
+//    
+//    public void moveFile(String fromGroup, String toGroup, DataFile file){
+//        UserFileGroup from = null;
+//        UserFileGroup to = null;
+//        for(int i = 0; i < fileGroups.size(); i++){
+//            if(fileGroups.get(i).getGroupName().equals(fromGroup)){
+//                from = fileGroups.get(i);
+//            }
+//            if(fileGroups.get(i).getGroupName().equals(toGroup)){
+//                to = fileGroups.get(i);
+//            }
+//        }
+//        if(from == null || to == null){
+//            return;
+//        }
+//        
+//        from.removeFile(file);
+//        to.addFile(file);
+//    }
+//
+//    public List<UserFileGroup> getFileGroups() {
+//        return fileGroups;
+//    }
 
     public String getName() {
         return name;
