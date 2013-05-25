da8a434c24fb02899bf27ca05447dc6ab5a86ab7
Zou Runyang <zry656565@yahoo.com.cn>
2013-05-25 23:01:30 +0800

    fix 2 bugs
    1. CLOB -> TEXT
    2. registerAction jump to wrong page.

---
+++ src/java/com/haijiao/presentation/action/RegisterThenAction.java
@@ -0,0 +1,139 @@
+/**
+ *
+ * @author Jerry Zou
+ */
+
+package com.haijiao.presentation.action;
+
+import com.haijiao.Domain.bean.Student;
+import com.haijiao.Domain.bean.Teacher;
+import com.haijiao.SupportService.service.IStudentService;
+import com.haijiao.SupportService.service.ITeacherService;
+
+public class RegisterThenAction extends SessionAction {
+    ITeacherService teacherService;
+    IStudentService studentService;
+    String id;
+    String password;
+    String name;
+    String sex;
+    String school;
+    String grade;
+    String province;
+    String net;
+    String nextPageMessage;
+    
+    public String teacherRegister(){
+        if(teacherService.changeInfo((String)this.getSessionValue("email"), name, sex, null, school, null, province, net)){
+            Teacher theTeacher = teacherService.getTeacherByEmail((String)this.getSessionValue("email"));
+            this.sessionPutIn("name", theTeacher.getName());
+            this.sessionPutIn("userType", "teacher");
+            this.sessionPutIn("todayClazz", teacherService.getTodayClasses((String)this.getSessionValue("email")));
+            return SUCCESS;
+        } else {
+            nextPageMessage = "注册信息提交失败！";
+            return "input";
+        }
+    }
+    
+    public String studentRegister(){
+        if(studentService.changeInfo((String)this.getSessionValue("email"), name, sex, null, grade, school, null, null)){
+            Student s = studentService.getStudentByEmail((String)this.getSessionValue("email"));
+            this.sessionPutIn("name", s.getName());
+            this.sessionPutIn("userType", "student");
+            this.sessionPutIn("todayClazz", studentService.getTodayClasses((String)this.getSessionValue("email")));
+            return SUCCESS;
+        } else {
+            nextPageMessage = "注册信息提交失败！";
+            return "input";
+        }
+    }
+
+    public ITeacherService getTeacherService() {
+        return teacherService;
+    }
+
+    public void setTeacherService(ITeacherService teacherService) {
+        this.teacherService = teacherService;
+    }
+
+    public IStudentService getStudentService() {
+        return studentService;
+    }
+
+    public void setStudentService(IStudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getNet() {
+        return net;
+    }
+
+    public void setNet(String net) {
+        this.net = net;
+    }
+
+    public String getNextPageMessage() {
+        return nextPageMessage;
+    }
+
+    public void setNextPageMessage(String nextPageMessage) {
+        this.nextPageMessage = nextPageMessage;
+    }
+}
