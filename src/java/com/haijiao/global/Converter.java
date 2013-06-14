0686edd7766b5c558bcfa48a96a25cc48420a1d3
zhang yang <zy3861@163.com>
2013-06-14 15:08:59 +0800

    converter

---
+++ src/java/com/haijiao/global/Converter.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.haijiao.global;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import org.artofsolving.jodconverter.OfficeDocumentConverter;
+import org.artofsolving.jodconverter.office.DefaultOfficeManagerConfiguration;
+import org.artofsolving.jodconverter.office.OfficeManager;
+
+/**
+ *
+ * @author hp
+ */
+public class Converter implements ServletContextListener{
+
+    public static final String PARAMETER_OFFICE_HOME = "D:\\Program Files (x86)\\openoffice.org 3";
+    private OfficeManager officeManager;
+    static OfficeDocumentConverter documentConverter;
+
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        officeManager = new DefaultOfficeManagerConfiguration().setOfficeHome(PARAMETER_OFFICE_HOME).buildOfficeManager();
+        documentConverter = new OfficeDocumentConverter(officeManager);
+        officeManager.start();
+    }
+    
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        officeManager.stop();
+    }
+
+    public static OfficeDocumentConverter getDocumentConverter(){
+        return documentConverter;
+    }
+}
